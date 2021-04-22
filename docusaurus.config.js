e5e1e6a9db6d2250fd8c4fd9aa21282b41fb3a63
Sai Karthik <kasaikarthik@gmail.com>
2021-04-22 11:04:09 +0530

    Modified docs landing page

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,7 +1,7 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'The tagline of my site',
+  title: 'Yblockchain Docs',
+  tagline: 'Say Yes to Blockchain',
   url: 'https://yblockcha.in',
   baseUrl: '/',
   onBrokenLinks: 'throw',
